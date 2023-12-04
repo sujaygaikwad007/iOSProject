55537b7ea73113b5794ab0526675606a5f872855
Aniket Patil <aniket.p@reapmind.com>
2023-12-04 10:22:04 +0530

    Initial Commit

---
+++ speechRecongizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  speechRecongizer
+//
+//  Created by Aniket Patil on 04/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
