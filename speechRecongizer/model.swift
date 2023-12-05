1b97cf46a283fdd0910886495a359f25696a76bc
Aniket Patil <aniket.p@reapmind.com>
2023-12-05 16:26:23 +0530

    Add model.Swift

---
+++ speechRecongizer/model.swift
@@ -0,0 +1,25 @@
+//
+//  model.swift
+//  speechRecongizer
+//
+//  Created by Aniket Patil on 05/12/23.
+//
+
+import Foundation
+
+let tableData = ["ऑर्डर स्थिति अपडेट कर दी गई है",
+                 "I'm not really a TV watcher.",
+                 "I'm not a big fan of snacking.",
+                 "George works on a small farm.",
+                 "I don’t like tea.",
+                 "It's so hot.","Thank you",
+                 " She was homeschooled until she was thirteen.",
+                 " A yardstick is 3 feet long.",
+                 " He had to wear the cast for two months.",
+                 " I am a vegan.",
+                 " I fear that he drinks too much.",
+                 "Anakin and Padme belong together.",
+                 " Daya chose a song from the list to sing.",
+                 " It drained down the toilet.",
+                 " Diamonds are forever.",
+                 " Captain America is my favorite Avenger."]
